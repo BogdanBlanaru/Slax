efacb50505b9b266d870d051d1a3b9c7ad64ae35
Bogdan <bogdansebastian200@yahoo.com>
2025-03-05 16:26:58 +0200

    add ChatRoomLive

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -4,6 +4,25 @@ defmodule SlaxWeb.ChatRoomLive do
   def render(assigns) do
     ~H"""
     <div>Welcome to the chat!</div>
+
+    <%!-- This is a comment
+    <div>{2 + 2}</div>
+
+    <div id={"person-#{person_id}"} class="person"></div>
+
+    <div class=["person", age >= 18 && "adult"]></div>
+
+    <div id="person">
+    <%= if person.age >= 18 do %>
+      <span>Adult</span>
+    <%= else %>
+       <span>Child</span>
+    <%= end %>
+    </div>
+
+    <div>
+    <span :if={person.age >= 18}>Adult</span>
+    </div> --%>
     """
   end
 end
