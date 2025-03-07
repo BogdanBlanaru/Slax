0c854389d0f119b7c7b26ff0b71c34c9079982e6
Bogdan <bogdansebastian200@yahoo.com>
2025-03-07 12:29:32 +0200

    show room info in ChatRoomLive

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -1,9 +1,25 @@
 defmodule SlaxWeb.ChatRoomLive do
   use SlaxWeb, :live_view
 
+  alias Slax.Chat.Room
+  alias Slax.Repo
+
   def render(assigns) do
     ~H"""
-    <div>Welcome to the chat!</div>
+    <div class="flex flex-col grow shadow-lg">
+      <div class="flex justify-between items-center shrink-0
+        h-16 bg-white border-b border-slate-300 px-4">
+        <div class="flex flex-col gap-1.5">
+          <h1 class="text-sm font-bold leading-none">
+            {@room.name}
+          </h1>
+          
+          <div class="text-xs leading-none h-3.5">
+            {@room.topic}
+          </div>
+        </div>
+      </div>
+    </div>
 
     <%!-- This is a comment
     <div>{2 + 2}</div>
@@ -24,5 +40,21 @@ defmodule SlaxWeb.ChatRoomLive do
     <span :if={person.age >= 18}>Adult</span>
     </div> --%>
     """
+  end
+
+  def mount(_params, _session, socket) do
+    # {:ok, socket}
+
+    room = Room |> Repo.all() |> List.first()
+    {:ok, assign(socket, :room, room)}
+
+    # socket =
+    #   socket
+    #   |> assign(:room, room)
+    #   |> assign(:director, "Tommy H.")
+
+    # {:ok, socket}
+
+    # socket = assign(socket, room: room, director: "Tommy H.")
   end
 end
