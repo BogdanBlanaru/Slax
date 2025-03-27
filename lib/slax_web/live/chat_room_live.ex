62c1fa3fb6d02e2a221494f789c54f3f38bae92d
Bogdan <bogdansebastian200@yahoo.com>
2025-03-27 15:33:12 +0200

    show user online status

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -1,9 +1,11 @@
 defmodule SlaxWeb.ChatRoomLive do
   use SlaxWeb, :live_view
 
+  alias Slax.Accounts
   alias Slax.Accounts.User
   alias Slax.Chat
   alias Slax.Chat.{Message, Room}
+  alias SlaxWeb.OnlineUsers
 
   def render(assigns) do
     ~H"""
@@ -23,6 +25,20 @@ defmodule SlaxWeb.ChatRoomLive do
 
         <div id="rooms-list">
           <.room_link :for={room <- @rooms} room={room} active={room.id == @room.id} />
+        </div>
+        <div class="mt-4">
+          <div class="flex items-center h-8 px-3">
+            <div class="flex items-center grow">
+              <span class="ml-2 leading-none font-medium text-sm">Users</span>
+            </div>
+          </div>
+          <div id="users-list">
+            <.user
+              :for={user <- @users}
+              user={user}
+              online={OnlineUsers.online?(@online_users, user.id)}
+            />
+          </div>
         </div>
       </div>
     </div>
@@ -115,25 +131,24 @@ defmodule SlaxWeb.ChatRoomLive do
         </.form>
       </div>
     </div>
+    """
+  end
 
-    <%!-- This is a comment
-    <div>{2 + 2}</div>
+  attr :user, User, required: true
+  attr :online, :boolean, default: false
 
-    <div id={"person-#{person_id}"} class="person"></div>
-
-    <div class=["person", age >= 18 && "adult"]></div>
-
-    <div id="person">
-    <%= if person.age >= 18 do %>
-      <span>Adult</span>
-    <%= else %>
-       <span>Child</span>
-    <%= end %>
-    </div>
-
-    <div>
-    <span :if={person.age >= 18}>Adult</span>
-    </div> --%>
+  defp user(assigns) do
+    ~H"""
+    <.link class="flex items-center h-8 hover:bg-gray-300 text-sm pl-8 pr-3" href="#">
+      <div class="flex justify-center w-4">
+        <%= if @online do %>
+          <span class="w-2 h-2 rounded-full bg-blue-500"></span>
+        <% else %>
+          <span class="w-2 h-2 rounded-full border-2 border-gray-500"></span>
+        <% end %>
+      </div>
+      <span class="ml-2 leading-none">{username(@user)}</span>
+    </.link>
     """
   end
 
@@ -192,10 +207,22 @@ defmodule SlaxWeb.ChatRoomLive do
 
   def mount(_params, _session, socket) do
     rooms = Chat.list_rooms()
+    users = Accounts.list_users()
 
     timezone = get_connect_params(socket)["timezone"]
 
-    {:ok, assign(socket, rooms: rooms, timezone: timezone)}
+    if connected?(socket) do
+      OnlineUsers.track(self(), socket.assigns.current_user)
+    end
+
+    OnlineUsers.subscribe()
+
+    socket =
+      socket
+      |> assign(rooms: rooms, timezone: timezone, users: users)
+      |> assign(online_users: OnlineUsers.list())
+
+    {:ok, socket}
   end
 
   def handle_params(params, _uri, socket) do
@@ -285,5 +312,11 @@ defmodule SlaxWeb.ChatRoomLive do
 
   def handle_info({:message_deleted, message}, socket) do
     {:noreply, stream_delete(socket, :messages, message)}
+  end
+
+  def handle_info(%{event: "presence_diff", payload: diff}, socket) do
+    online_users = OnlineUsers.update(socket.assigns.online_users, diff)
+
+    {:noreply, assign(socket, online_users: online_users)}
   end
 end
